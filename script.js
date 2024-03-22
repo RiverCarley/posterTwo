1a69b0ec69afff0afc2f1088dcef36daec6c8e15
RiverCarley <river.carley@ocadu.ca>
2024-03-22 14:47:38 -0400

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".speaker").click(function(){
+            
+            
+            
+    if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(".container").removeClass("active");
+    } else {
+        $(".speaker").removeClass("active");
+        $(this).addClass("active");
+        $(".container").addClass("active");
+    }
+    
+    
+
+    
+})
